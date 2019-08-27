d3b590b184bd99f7a7e7072fa2c53ca4d0ed2bd5
vpashencev <antiguru110894@gmail.com>
2019-08-28 00:49:08 +0300

    create eda notebook, fix config

--- src/config.py
+++ src/config.py
@@ -5,8 +5,8 @@ submissions_path = '../output/'
 models_path = '../models/'
 tmp_path = '../tmp/'
 
-train_transaction = os.path.join(input_path, 'train_transaction.csv')
-train_identify = os.path.join(input_path, 'train_identity.csv')
+train_transactions_path = os.path.join(input_path, 'train_transaction.csv')
+train_identify_path = os.path.join(input_path, 'train_identity.csv')
 
-test_transaction = os.path.join(input_path, 'test_transaction.csv')
-test_identify = os.path.join(input_path, 'test_identity.csv')
+test_transactions_path = os.path.join(input_path, 'test_transaction.csv')
+test_identify_path = os.path.join(input_path, 'test_identity.csv')
